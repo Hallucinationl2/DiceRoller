1f7ff1caf98073b2c3aa52f8798164941f789aee
Radoslav <117578667+Hallucinationl2@users.noreply.github.com>
2025-05-30 23:40:02 +0300

    Add files via upload

---
+++ DiceRoller/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class DiceRoller
+{
+    static void Main()
+    {
+        Random rng = new Random();
+        string input;
+        do
+        {
+            Console.Write("Натиснете Enter за да хвърлите зар или 'q' за изход: ");
+            input = Console.ReadLine();
+            if (input.ToLower() != "q")
+            {
+                int roll = rng.Next(1, 7);
+                Console.WriteLine($"Хвърлихте: {roll}\n");
+            }
+        } while (input.ToLower() != "q");
+    }
+}
